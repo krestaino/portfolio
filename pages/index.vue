23d62fc5bf19a526dd805a5d8205ff3a1a875d73
Kevin Restaino <kevinrestaino@gmail.com>
2017-10-31 07:39:50 -0500

    feat(home): show project details in cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="work">
     <ul class="projects">
-      <li v-for="project in projects">
-        <router-link tag="a" :to="`/work/${project.category}/${project.slug}`">
-          <h3>{{ project.title }}</h3>
-          <div class="image-container lazy">
-            <img v-lazy="`/work/${project.category}/${project.slug}/${project.slug}_thumb@2x.png`"/>
-            <div class="spinner"></div>
+      <li v-for="project in projects" :key="project.id">
+        <article>
+          <router-link tag="a" :to="`/work/${project.category}/${project.slug}`">
+            <span class="hover-text">{{ project.title }}</span>
+            <div class="image-container lazy">
+              <img class="thumb" v-lazy="`/work/${project.category}/${project.slug}/${project.slug}_thumb@2x.png`"/>
+              <div class="spinner"></div>
+            </div>
+          </router-link>
+          <div class="details">
+            <h3>{{ project.title }}</h3>
+            <div>
+              <router-link tag="a" :to="project.github" class="button" target="_blank" v-if="project.github">GitHub <img src="~/assets/icons/github.svg"></router-link>
+              <router-link tag="a" :to="project.url" class="button" target="_blank" v-if="project.url">Visit Site <img src="~/assets/icons/ic_open_in_new_black_24px.svg"></router-link>
+              <router-link tag="a" :to="`/work/${project.category}/${project.slug}`" class="button">View Project</router-link>
+            </div>
           </div>
-        </router-link>
+        </article>
       </li>
     </ul>
   </div>
@@ -33,17 +43,22 @@ export default {
 
   li {
     width: 100%;
-
+    
     & + li {
       margin-top: 36px;
     }
+  }
 
-    a {
+  article {
+    box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.25);
+    width: 100%;
+
+    > a {
       display: block;
       position: relative;
     }
 
-    h3 {
+    .hover-text {
       color: #fff;
       font-size: 28px;
       font-weight: 100;
@@ -76,22 +91,41 @@ export default {
       }
     }
 
-    img {
+    .thumb {
       display: block;
       height: 100%;
       height: 450px;
 
       &[lazy="loaded"] {
-        box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.25);
-
         & + .spinner {
           display: none;
         }
       }
     }
 
-    a:hover {
+    .details {
+      align-items: center;
+      background-color: #fff;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%;
+
       h3 {
+        flex: 1;
+      }
+
+      .button {
+        display: inline-flex;
+
+        & + .button {
+          margin-left: 8px;
+        }
+      }
+    }
+
+    > a:hover {
+      .hover-text {
         opacity: 1;
         transform: translateY(0);
       }
