e6658c6b2eaa7482b3e641afa1ec7e5f72251871
Kevin Restaino <kevinrestaino@gmail.com>
2018-05-06 08:04:54 -0500

    refactor(apollo): remove unused client-config code

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -1,25 +1,9 @@
-import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-export default (ctx) => {
-  const uri = ctx.isDev ? process.env.API_DEV : process.env.API_PROD
-  const httpLink = new HttpLink({ uri: uri + '/graphql' })
-
-  // middleware
-  const middlewareLink = new ApolloLink((operation, forward) => {
-    // This function is called before every request. Update ctx.req.session and window.__NUXT__.state.session
-    // To point to wherever you store your token
-    // const token = process.server ? ctx.req.session : window.__NUXT__.state.session
-
-    operation.setContext({
-      headers: {
-        // authorization: `Bearer ${token}`
-      }
-    })
-    return forward(operation)
-  })
-  const link = middlewareLink.concat(httpLink)
+export default (contex) => {
+  const uri = contex.isDev ? process.env.API_DEV : process.env.API_PROD
+  const link = new HttpLink({ uri: uri + '/graphql' })
   return {
     link,
     cache: new InMemoryCache()
